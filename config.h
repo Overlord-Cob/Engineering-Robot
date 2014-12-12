49fad99f092ab901be4ce884d414c7952650c7e6
Overlord-Cob <chase@chaseblock.com>
2014-12-12 09:18:47 -0600

    Create config.h
    The costant values for the robot's code

---
+++ config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DEADBAND_THRESHOLD 10.0
+#define DEADBAND(x) (abs(x) >= DEADBAND_THRESHOLD ? x : 0)
+
+#define BTN_JOYSTICK_MOVE_SPEED 128
+#define BTN_MOTOR_SPEED 127
+
+#define JOY_BTN_LEFT Ch3
+#define JOY_BTN_RIGHT Ch2
+
+#define BTN_ARM_UP Btn8U
+#define BTN_ARM_DOWN Btn8D
+
+#define BTN_CLAW_TOGGLE Btn6U  //Currently unused
+#define BTN_CLAW_OPEN Btn7U
+#define BTN_CLAW_CLOSE Btn7D
+
+#endif
